769e57bc805e41a7fd0b03237dfa82b2a1dd1c6b
https://github.com/nguyenaty2004 <bn@v_d.com>
2023-08-10 16:35:32 +0700

    10/8/2023

---
+++ src/BT_JDBC_ONTAP/ConnectJDBC.java
@@ -0,0 +1,10 @@
+package BT_JDBC_ONTAP;
+
+public class ConnectJDBC {
+    private String hostName = "localhost:3306";
+    private String dbName = "demo_jdbc";
+    private String username = "OOP3_";
+    private String password = "1";
+
+
+}
